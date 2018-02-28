3335c3f8e2d912be697537aa63b278b17bb239d1
Jose Mejia <jrmejiasoft@gmail.com>
2018-02-28 11:49:16 -0400

    Implementing Logging logic.

--- Cobinhood.API.Csharp.Client/ApiClient.cs
+++ Cobinhood.API.Csharp.Client/ApiClient.cs
@@ -11,36 +11,41 @@ using System.Collections.Generic;
 using AutoMapper;
 using Newtonsoft.Json.Linq;
 using System.Linq;
+using Cobinhood.API.Csharp.Client.Configuration;
 
 namespace Cobinhood.API.Csharp.Client
 {
     public class ApiClient : ApiClientAbstract, IApiClient
     {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
         /// <see cref="ApiClientAbstract(string, string, string, string, bool)"/>
         public ApiClient(string apiKey, string apiUrl = @"https://api.cobinhood.com", string webSocketEndpoint = @"wss://feed.cobinhood.com/ws", bool addDefaultHeaders = true) : base(apiKey, apiUrl, webSocketEndpoint, addDefaultHeaders)
         {
+            NlogConfig.Configure();
         }
 
         /// <see cref="IApiClient.CallAsync{T}(ApiMethod, string, bool, string)"/>
         public async Task<T> CallAsync<T>(ApiMethod method, string endpoint, string parameters = null, object data = null)
         {
-            var finalEndpoint = endpoint + (string.IsNullOrWhiteSpace(parameters) ? "" : $"?{parameters}");
-
-            if (method != ApiMethod.GET)
-            {
-                _httpClient.DefaultRequestHeaders.Remove("nonce");
-                _httpClient.DefaultRequestHeaders.Add("nonce", DateTime.Now.GetUnixTimeStamp());
-            }
-
-            var request = new HttpRequestMessage(Utilities.CreateHttpMethod(method.ToString()), finalEndpoint);
-
-            if (data != null)
-            {
-                request.Content = new StringContent(JsonConvert.SerializeObject(data));
-            }
-
             try
             {
+                var finalEndpoint = endpoint + (string.IsNullOrWhiteSpace(parameters) ? "" : $"?{parameters}");
+
+                if (method != ApiMethod.GET)
+                {
+                    _httpClient.DefaultRequestHeaders.Remove("nonce");
+                    _httpClient.DefaultRequestHeaders.Add("nonce", DateTime.Now.GetUnixTimeStamp());
+                }
+
+                var request = new HttpRequestMessage(Utilities.CreateHttpMethod(method.ToString()), finalEndpoint);
+
+                if (data != null)
+                {
+                    request.Content = new StringContent(JsonConvert.SerializeObject(data));
+                }
+
+
                 var response = await _httpClient.SendAsync(request).ConfigureAwait(false);
 
                 // Get the result
@@ -51,57 +56,67 @@ namespace Cobinhood.API.Csharp.Client
             }
             catch (Exception ex)
             {
+                logger.Error(ex, "An error has occurred!");
                 throw ex;
             }
         }
 
         public void SuscribeToWebSocket<T>(MessageHandler<T> messageDelegate, object data = null)
         {
-            var finalEndpoint = _webSocketEndpoint;
-
-            var ws = new WebSocket(finalEndpoint)
+            try
             {
-                CustomHeaders = new Dictionary<string, string>
+                var finalEndpoint = _webSocketEndpoint;
+
+                var ws = new WebSocket(finalEndpoint)
                 {
-                    {"authorization", _apiKey},
-                    {"nonce", DateTime.Now.GetUnixTimeStamp()}
+                    CustomHeaders = new Dictionary<string, string>
+                    {
+                        {"authorization", _apiKey},
+                        {"nonce", DateTime.Now.GetUnixTimeStamp()}
+                    }
+                };
+
+                ws.OnMessage += (sender, e) =>
+                {
+                    var responseData = JsonConvert.DeserializeObject<JToken>(e.Data);
+                    var eventData = Mapper.Map<T>(responseData);
+                    messageDelegate(eventData);
+                };
+
+                ws.OnClose += (sender, e) =>
+                {
+                    _openSockets.Remove(ws);
+                };
+
+                ws.OnError += (sender, e) =>
+                {
+                    _openSockets.Remove(ws);
+                    logger.Error(e.Exception, "An error has occurred!");
+                    throw e.Exception;
+                };
+
+                ws.Connect();
+
+                if (data != null)
+                {
+                    var serializedData = JsonConvert.SerializeObject(data, Formatting.Indented, new JsonSerializerSettings
+                    {
+                        NullValueHandling = NullValueHandling.Ignore,
+                        DefaultValueHandling = DefaultValueHandling.Ignore
+                    });
+
+                    var finalData = serializedData;
+
+                    ws.Send(finalData);
                 }
-            };
 
-            ws.OnMessage += (sender, e) =>
-            {
-                var responseData = JsonConvert.DeserializeObject<JToken>(e.Data);
-                var eventData = Mapper.Map<T>(responseData);
-                messageDelegate(eventData);
-            };
-
-            ws.OnClose += (sender, e) =>
-            {
-                _openSockets.Remove(ws);
-            };
-
-            ws.OnError += (sender, e) =>
-            {
-                _openSockets.Remove(ws);
-            };
-
-            ws.Connect();
-
-            if (data != null)
-            {
-                var serializedData = JsonConvert.SerializeObject(data, Formatting.Indented, new JsonSerializerSettings
-                {
-                    NullValueHandling = NullValueHandling.Ignore,
-                    DefaultValueHandling = DefaultValueHandling.Ignore
-                });
-
-                var finalData = serializedData;
-
-                ws.Send(finalData);
-
+                _openSockets.Add(ws);
             }
-
-            _openSockets.Add(ws);
+            catch (Exception ex)
+            {
+                logger.Error(ex, "An error has occurred!");
+                throw ex;
+            }
         }
     }
 }
---
+++ Cobinhood.API.Csharp.Client/Configuration/NlogConfig.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cobinhood.API.Csharp.Client.Configuration
+{
+    public static class NlogConfig
+    {
+        public static void Configure() {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            var fileTarget = new NLog.Targets.FileTarget() { FileName = "${basedir}/log.txt", Name = "logfile", Layout = "${longdate} ${message} ${exception:format=tostring}" };
+            config.LoggingRules.Add(new NLog.Config.LoggingRule("*", LogLevel.Trace, fileTarget));
+
+            LogManager.Configuration = config;
+        }
+    }
+}
